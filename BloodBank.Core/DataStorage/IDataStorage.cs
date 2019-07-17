a9a84a45c67fd3ac471466721a80f6d9248619d1
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-07-17 20:37:29 +0200

    Added IDataStorage

---
+++ BloodBank.Core/DataStorage/IDataStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BloodBank.Core.DataStorage
+{
+    public interface IDataStorage
+    {
+        void Serialize<T>(string path, List<T> type);
+        List<T> Deserialize<T>(string tableName, T type);
+    }
+}
---
+++ BloodBank.Storage/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BloodBank.Core.DataStorage;
+using LiteDB;
+
+namespace BloodBank.Storage
+{
+    public class Storage : IDataStorage
+    {
+        public void Serialize<T>(string tableName, List<T> objects)
+        {
+            using (var database = new LiteDatabase("@BloodBank.db"))
+            {
+                var data =  database.GetCollection<T>(tableName);
+                var validData = objects.Except(data.FindAll().ToList());
+                data.InsertBulk(validData);
+            }
+        }
+
+        public List<T> Deserialize<T>(string tableName, T objects)
+        {
+            using (var database = new LiteDatabase("@BloodBank.db"))
+            {
+                return database.GetCollection<T>(tableName).FindAll().ToList();
+            }
+        }
+    }
+}
