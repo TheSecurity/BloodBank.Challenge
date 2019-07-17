39a7d316e8d21e08de5d7a740d57ca513b5b0688
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-07-17 21:49:12 +0200

    added missing static keyword

--- BloodBank.Challenge/Program.cs
+++ BloodBank.Challenge/Program.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BloodBank.ConsoleApp
 {
     public class Program
     {
-        public async void Main()
+        public static async Task Main()
         {
             var services = DependencyContainer.SetupServiceProvider;
             await services.GetRequiredService<UserInterface>().Initialize();
