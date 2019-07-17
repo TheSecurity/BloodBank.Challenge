35161e8678348b25b979680005b311d20cb1ffb6
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-07-17 20:47:01 +0200

    introduced Provider

---
+++ BloodBank.Core/DataStorage/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace BloodBank.Core.DataStorage
+{
+    public interface IDataProvider
+    {
+        T GetEntity<T>(string tableName, ulong id);
+        int GetCount<T>(string tableName);
+    }
+}
---
+++ BloodBank.Storage/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using BloodBank.Core.DataStorage;
+using LiteDB;
+
+namespace BloodBank.Storage
+{
+    public class Provider : IDataProvider
+    {
+        public T GetEntity<T>(string tableName, ulong id)
+        {
+            using (var database = new LiteDatabase("@BloodBank.db"))
+            {
+                var data = database.GetCollection<T>(tableName);
+                return data.FindById(id);
+            }
+        }
+
+        public int GetCount<T>(string tableName)
+        {
+            using (var database = new LiteDatabase("@BloodBank.db"))
+            {
+                var data = database.GetCollection<T>(tableName);
+                return data.Count();
+            }
+        }
+    }
+}
