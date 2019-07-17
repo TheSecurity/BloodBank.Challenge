ca0a10db28dd9ed0024def3efee6c9026fa12441
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-07-17 11:49:40 +0200

    Aded Dependency Injection Container

---
+++ BloodBank.Challenge/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BloodBank.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodBank.ConsoleApp
+{
+    public class DependencyContainer
+    {
+        public static IServiceProvider SetupServiceProvider
+            => new ServiceCollection()
+                .AddSingleton<UserInterface>()
+                .AddSingleton<IPersonService, PersonService>()
+                .BuildServiceProvider();
+    }
+}
