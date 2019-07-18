bd929b40932dc05048774a14e70993c4888c2d9c
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-07-18 12:02:37 +0200

    Added getting and adding new user

--- BloodBank.Challenge/UserInterface.cs
+++ BloodBank.Challenge/UserInterface.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using BloodBank.Core.Entities;
 using BloodBank.Core.Services;
+using Console = System.Console;
 
 namespace BloodBank.ConsoleApp
 {
     public class UserInterface
     {
         private readonly IPersonService _personService;
-        public UserInterface(IPersonService personService)
+        private readonly PersonPropertyValidator _propertyValidator;
+        public UserInterface(IPersonService personService, PersonPropertyValidator propertyValidator)
         {
             _personService = personService;
+            _propertyValidator = propertyValidator;
         }
 
         public Task Initialize()
@@ -26,12 +29,14 @@ namespace BloodBank.ConsoleApp
         private static void DrawUserInterface()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Welcome to Blood bank!\n\n" +
+            Console.WriteLine("\n\n\n=====================================\n" +
+                              "Welcome to Blood bank!\n\n" +
                               "Please select option by its number:\n" +
                               "1. Register person\n" +
                               "2. Find person\n" +
                               "3. Person count\n" +
-                              "4. Close\n\n");
+                              "4. Close\n\n" +
+                              "=====================================\n");
         }
 
         private void HandleConsoleInput()
@@ -51,68 +56,118 @@ namespace BloodBank.ConsoleApp
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Wrong option name!");
                         Console.ForegroundColor = ConsoleColor.Green;
+                        DrawUserInterface();
                         break;
                 }
                 input = Console.ReadLine();
             }
         }
 
-        private Task AddPersonDialog()
+        private void AddPersonDialog()
         {
             var person = new Person();
 
+            Console.WriteLine("===== REGISTER NEW PERSON =====");
+
             Console.Write("Firstname: ");
-            person.Firstname = Console.ReadLine();
+            var firstName = _propertyValidator.FirstnameValidation(Console.ReadLine());
+            if (firstName.ErrorMessage != null)
+            {
+                ErrorOccured(firstName.ErrorMessage);
+                return;
+            }
+            person.Firstname = firstName.Value.ToString();
 
             Console.Write("Surname: ");
-            person.Surname = Console.ReadLine();
+            var surname = _propertyValidator.SurnameValidation(Console.ReadLine());
+            if (firstName.ErrorMessage != null)
+            {
+                ErrorOccured(surname.ErrorMessage);
+                return;
+            }
+            person.Surname = surname.Value.ToString();
 
             Console.Write("Personal Identification Number: ");
-            if (!ulong.TryParse(Console.ReadLine(), out var personalId))
+            var personalId = _propertyValidator.PersonalIdValidation(Console.ReadLine());
+            if (personalId.ErrorMessage != null)
             {
-                Console.WriteLine("Number is invalid!");
-                return Task.CompletedTask;
+                ErrorOccured(surname.ErrorMessage);
+                return;
             }
-            person.PersonalIdentificationNumber = personalId;
+            person.PersonalIdentificationNumber = (ulong)personalId.Value;
 
             Console.Write("Date of birth (dd-mm-yyyy): ");
-            if(!DateTime.TryParse(Console.ReadLine(), out var datetime))
+            var dateOfBirth = _propertyValidator.DateOfBirthValidation(Console.ReadLine());
+            if (dateOfBirth.ErrorMessage != null)
             {
-                Console.WriteLine("Failed parsing to format dd-mm-yyyy");
-                return Task.CompletedTask;
+                ErrorOccured(dateOfBirth.ErrorMessage);
+                return;
             }
-            person.DateOfBirth = datetime;
+            person.DateOfBirth = (DateTime)dateOfBirth.Value;
 
             Console.Write("Address: ");
-            person.Address = Console.ReadLine();
+            var address = _propertyValidator.AddressValidation(Console.ReadLine());
+            if (address.ErrorMessage != null)
+            {
+                ErrorOccured(address.ErrorMessage);
+                return;
+            }
+            person.Address = address.Value.ToString();
 
             Console.Write("Country: ");
-            person.Country = Console.ReadLine();
-
-            Console.Write("BloodType select option: ");
-            var bloodTypes = Enum.GetValues(typeof(BloodType)).Cast<string>().ToList();
-            for (var i = 0; i < bloodTypes.Count-1; i++)
+            var country = _propertyValidator.CountryValidation(Console.ReadLine());
+            if (country.ErrorMessage != null)
             {
-                Console.Write($"{i+1}. {bloodTypes[i]}");
+                ErrorOccured(country.ErrorMessage);
+                return;
             }
+            person.Country = (string)country.Value;
 
-            if (!Enum.TryParse(typeof(BloodType), Console.ReadLine(), out var bloodType))
+            Console.WriteLine("Blood types select option: ");
+            var bloodTypes = Enum.GetValues(typeof(BloodType)).Cast<BloodType>().ToList();
+            for (var i = 0; i < bloodTypes.Count - 1; i++)
             {
-                Console.WriteLine("Invalid value!");
-                return Task.CompletedTask;
+                Console.Write($"{i + 1}. {bloodTypes[i]}\n");
             }
-
-            person.BloodInfo.BloodType = (BloodType)bloodType;
-
-            //_personService.AddPerson(person);
-
-            return Task.CompletedTask;
+            var bloodType = _propertyValidator.BloodTypeValidation(Console.ReadLine(), bloodTypes);
+            if (bloodType.ErrorMessage != null)
+            {
+                ErrorOccured(bloodType.ErrorMessage);
+                return;
+            }
+            person.BloodInfo = (BloodInfo)bloodType.Value;
+            
+            _personService.AddPerson(person, person.PersonalIdentificationNumber);
         }
 
-        private Task GetPersonDialog()
+        private void GetPersonDialog()
         {
+            Console.WriteLine("\n\n===== GET PERSON =====");
 
-            return Task.CompletedTask;
+            Console.Write("Personal Identification Number: ");
+
+            if (!ulong.TryParse(Console.ReadLine(), out var result))
+            {
+                ErrorOccured("Invalid number inserted.");
+                return;
+            }
+
+            var person = _personService.GetPerson(result);
+
+            Console.WriteLine("\n\n==== Personal Details ====\n" +
+                              $"Firstname: {person.Firstname}\n" +
+                              $"Surname: {person.Surname}\n" +
+                              $"Personal Identification Number: {person.PersonalIdentificationNumber}\n" +
+                              $"Date of birth: {person.DateOfBirth.ToString(CultureInfo.InvariantCulture)}\n" +
+                              $"Address: {person.Address}\n" +
+                              $"Country: {person.Country}\n" +
+                              $"Blood type: {person.BloodInfo.BloodType}");
+        }
+
+        private static void ErrorOccured(string text)
+        {
+            Console.WriteLine(text);
+            DrawUserInterface();
         }
     }
 }
